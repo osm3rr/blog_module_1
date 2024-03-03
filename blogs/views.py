e2183d0c7d4c52c839011feced0bdbc91f27eeed
osm3rr <osm3rr@gmail.com>
2024-03-03 19:24:45 -0400

    list view added

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Article
 
-# Create your views here.
+
+# Create your views here : ).
+
+class ArticleListView(ListView):
+    model = Article
+    template_name = "home.html"

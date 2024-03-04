15c93bb62962b96e89fcc24e90a04aeea253c9ba
osm3rr <osm3rr@gmail.com>
2024-03-03 20:24:49 -0400

    detail view added

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ArticleListView
+from .views import ArticleListView, ArticleDetailView
 
 urlpatterns = [
     path("", ArticleListView.as_view(), name="home"),
+    path("article/<int:pk>/", ArticleDetailView.as_view(), name="post_detail"),
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Article
 
 
@@ -8,3 +8,7 @@ from .models import Article
 class ArticleListView(ListView):
     model = Article
     template_name = "home.html"
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = "post_detail.html"

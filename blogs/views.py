d0bfe277c528711e77ea02b036d2e45602765050
osm3rr <osm3rr@gmail.com>
2024-03-03 21:44:48 -0400

    article delete

--- blogs/views.py
+++ blogs/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Article
+from django.urls import reverse_lazy
 
 
 # Create your views here : ).
@@ -23,3 +24,8 @@ class ArticleUpdateView(UpdateView):
     model = Article
     template_name = "article_edit.html"
     fields = ['title', 'body']
+
+class ArticleDeleteView(DeleteView):
+    model = Article
+    template_name = "article_delete.html"
+    success_url = reverse_lazy("home")
